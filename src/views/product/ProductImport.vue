4839e9ae291f3391208e43eb43f661b3ce2c1ac4
lu.com <g956509785@163.com>
2020-06-26 10:34:27 +0800

    新增上传功能

--- src/views/product/ProductImport.vue
+++ src/views/product/ProductImport.vue
@@ -1,13 +1,19 @@
+<!-- 单文件上传组件 -->
 <template>
-    <div>商品导入</div>
+    <div>
+        <el-upload
+                class="upload-demo"
+                drag
+                action="http://192.168.2.102:9090/proImp/upload"
+                multiple
+                name="file">
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+<!--            <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>-->
+        </el-upload>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: "AllProduct"
-    }
+
 </script>
-
-<style scoped>
-
-</style>
